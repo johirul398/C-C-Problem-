9738450f14ed210ae93d9f1945c49afe0fd7aaab
johirul398 <mdjohirulislam545@gmail.com>
2021-07-01 22:09:37 +0600

    source code

---
+++ bk.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;//="Welcome to 30 Days of Code!";
+    gets(s);
+    cout<<"Hello, World."<<endl<<s<<endl;
+   // return 0;
+}
+
