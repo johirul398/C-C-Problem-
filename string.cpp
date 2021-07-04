796753ff371485ae1b6fdf634f4f51a16651c748
johirul398 <mdjohirulislam545@gmail.com>
2021-07-04 19:48:03 +0600

    source code

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j,k;
+    string s;
+    cin>>s;
+    while(s[i]!='a'&&'e'&&'i'&&'o'&&'u'&&'A'&&'E'&&'I'&&'O'&&'U')
+      {
+          cout<<'.'<<s[i];
+      }
+
+    return 0;
+}
